51009fd363dced9ed68c7def6ea24876ec72adfd
ANIKET BISWAS <aniketbis9680@gmail.com>
2026-03-04 13:46:11 +0530

    initial commit

--- server.py
+++ server.py
@@ -1,5 +1,28 @@
 from fastapi import FastAPI , HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from httpcore import request
 from pydantic import BaseModel
 import ollama
+from pyexpat.errors import messages
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+class ChatRequest(BaseModel):
+    message: str
+
+@app.post("/chat")
+async def chat(request: ChatRequest):
+    try:
+        response = ollama.chat(model="qwen2.5:7b" , messages = [{"role": "user","content": request.message}])
+        return {"response": response["message"]["content"]}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+@app.get("/")
+def home():
+    return {"message": "Qwen 2.5 API is working"}
